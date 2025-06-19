a889477199543efa67ad2d66f39966c5d20a34ce
padmabharathi-ailaysa <padmabharathi@ailaysa.net>
2025-06-19 13:30:50 +0530

    Format code changes

--- src/App.jsx
+++ src/App.jsx
@@ -271,7 +271,7 @@ function App() {
                 AppsCardsInfo.push(each.apps_template_cards.data.flat(1));
             });
             dispatch(setIndividualTemplateList({
-                res: AppsCardsInfo?.flat(1);
+                res: AppsCardsInfo?.flat(1)
             }));
             // setDataFilterItem(AppsCardsInfo?.flat(1));
             // DataFilterItem.current = AppsCardsInfo?.flat(1);
